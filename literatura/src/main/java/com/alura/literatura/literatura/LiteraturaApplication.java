36d35d4db856a8c7690d59e2b89ee69bf0c37f07
valeriasouza28 <82419677+valeriasouza28@users.noreply.github.com>
2024-06-16 14:52:36 -0300

    Update LiteraturaApplication.java

--- literatura/src/main/java/com/alura/literatura/literatura/LiteraturaApplication.java
+++ literatura/src/main/java/com/alura/literatura/literatura/LiteraturaApplication.java
@@ -1,4 +1,4 @@
-package com.alura.literatura.literatura;
+upackage com.alura.literatura.literatura;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
